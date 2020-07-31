2df519ecd42ca493116dcf5d392b92202db83d8c
Mario Sanchez Prada <mario@igalia.com>
2020-07-31 11:45:22 +0200

    Update calls to base::DeleteFile(path, recursive) to new APIs
    Calls to DeleteFile(path, true) become DeleteFileRecursively(path),
    and calls to DeleteFile(path, falsee) become DeleteFile(path).
    
    Then, DeleteFileRecursively became DeletePathRecursively.
    
    Chromium changes:
    
    https://chromium.googlesource.com/chromium/src/+/9382efe14df0303a66a78e6dee4ee8f91436d994
    
    commit 9382efe14df0303a66a78e6dee4ee8f91436d994
    Author: Lei Zhang <thestig@chromium.org>
    Date:   Sat Jul 25 20:52:14 2020 +0000
    
        Remove deprecated 2-param base::DeleteFile().
    
        Convert the few remaining callers within base/.
    
        Bug: 1009837
    
    https://chromium.googlesource.com/chromium/src/+/746ce47465d3549e5345c5986bf4b92e80d6f0bb
    
    commit 746ce47465d3549e5345c5986bf4b92e80d6f0bb
    Author: Lei Zhang <thestig@chromium.org>
    Date:   Wed Jul 1 04:39:45 2020 +0000
    
        Rename base::DeleteFileRecursively() to DeletePathRecursively().
    
        Take the name suggestion from several reviewers while converting
        base::DeleteFile(path, true) calls.
    
        Tbr: blundell@chromium.org,jamiewalch@chromium.org,jkarlin@chromium.org,kinuko@chromium.org,reillyg@chromium.org,sdefresne@chromium.org,xdai@chromium.org
        Bug: 1009837

--- components/brave_rewards/browser/rewards_service_impl.cc
+++ components/brave_rewards/browser/rewards_service_impl.cc
@@ -1427,7 +1427,7 @@ bool RewardsServiceImpl::ResetOnFilesTaskRunner() {
 
   bool res = true;
   for (size_t i = 0; i < paths.size(); i++) {
-    if (!base::DeleteFileRecursively(paths[i])) {
+    if (!base::DeletePathRecursively(paths[i])) {
       res = false;
     }
   }
@@ -2279,7 +2279,7 @@ bool RewardsServiceImpl::ClearDiagnosticLogOnFileTaskRunner(
 
   diagnostic_log_.Close();
 
-  return base::DeleteFile(path, false);
+  return base::DeleteFile(path);
 }
 
 void RewardsServiceImpl::OnClearDiagnosticLogOnFileTaskRunner(

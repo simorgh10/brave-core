5faf85833ba7f00c461286d648b2647fc7b72715
Simon Hong <shong@brave.com>
2020-09-09 15:19:14 +0900

    Delete SR component data caching logic
    As we decided not show SR top sites in NTP after SR is terminated,
    we don't need to cache it.
    
    Delete getDefaultSuperReferralTopSites api and related code

--- components/brave_new_tab_ui/api/initialData.ts
+++ components/brave_new_tab_ui/api/initialData.ts
@@ -16,7 +16,6 @@ export type InitialData = {
   privateTabData: privateTabDataAPI.PrivateTabData
   torTabData: torTabDataAPI.TorTabData
   topSites: chrome.topSites.MostVisitedURL[]
-  defaultSuperReferralTopSites: undefined | NewTab.DefaultSuperReferralTopSite[]
   brandedWallpaperData: undefined | NewTab.BrandedWallpaper
   togetherSupported: boolean
   geminiSupported: boolean
@@ -47,7 +46,6 @@ export async function getInitialData (): Promise<InitialData> {
       privateTabData,
       torTabData,
       topSites,
-      defaultSuperReferralTopSites,
       brandedWallpaperData,
       togetherSupported,
       geminiSupported
@@ -57,7 +55,6 @@ export async function getInitialData (): Promise<InitialData> {
       privateTabDataAPI.getPrivateTabData(),
       torTabDataAPI.getTorTabData(),
       topSitesAPI.getTopSiteTiles(),
-      !isIncognito ? brandedWallpaper.getDefaultSuperReferralTopSites() : Promise.resolve(undefined),
       !isIncognito ? brandedWallpaper.getBrandedWallpaper() : Promise.resolve(undefined),
       new Promise((resolve) => {
         if (!('braveTogether' in chrome)) {
@@ -81,7 +78,6 @@ export async function getInitialData (): Promise<InitialData> {
       privateTabData,
       torTabData,
       topSites,
-      defaultSuperReferralTopSites,
       brandedWallpaperData,
       togetherSupported,
       geminiSupported
